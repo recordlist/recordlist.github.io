8c0a07d572085ab57280db52cbaf12fd9968cc36
Roman Pasiar <romanp@idarwin.bratislava-einsteinova.sk.ibm.com>
2020-01-30 17:35:11 +0100

    after clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,72 @@
 // Load plugins
 const browsersync = require("browser-sync").create();
+const del = require("del");
 const gulp = require("gulp");
 const gulp_uglify = require("gulp-uglify");
 var concat = require('gulp-concat');
-
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function(cb) {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-  // modernizr + respond
-  gulp.src([
-    './node_modules/modernizr/dist/modernizr-3.8.0.min.js',
-    './node_modules/respond.js/dest/respond.min.js'
-  ])
-  .pipe(concat('modernizr-3.8.0-respond-1.4.2.min.js'))
-  .pipe(gulp.dest('./vendor/modernizr'))
-
-  cb();
-
-});
+const merge = require("merge-stream");
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
       baseDir: "./"
-    }
+    },
+    port: 3000
   });
   done();
 }
 
-// BrowserSync Reload
+// BrowserSync reload
 function browserSyncReload(done) {
   browsersync.reload();
   done();
 }
 
+// Clean vendor
+function clean() {
+  return del(['./vendor/*']);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap
+  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap'));
+
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+
+  // modernizr + respond
+  var modernizr = gulp.src([
+      './node_modules/modernizr/dist/modernizr-3.8.0.min.js',
+      './node_modules/respond.js/dest/respond.min.js'
+    ])
+    .pipe(concat('modernizr-3.8.0-respond-1.4.2.min.js'))
+    .pipe(gulp.dest('./vendor/modernizr'));
+
+  return merge(bootstrap, jquery, modernizr);
+}
+
 // Watch files
 function watchFiles() {
-  gulp.watch("./css/*", browserSyncReload);
+  gulp.watch("./**/*.css", browserSyncReload);
   gulp.watch("./**/*.html", browserSyncReload);
 }
 
-gulp.task("default", gulp.parallel('vendor'));
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-// dev task
-gulp.task("dev", gulp.parallel(watchFiles, browserSync));
+// Export tasks
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.dev = watch;
+exports.default = build;
