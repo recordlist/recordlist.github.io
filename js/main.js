25d85d28247614f6345ea0b130187cd6ee0ac0ac
peterkocis2 <tex.bates@mail.com>
2021-01-16 12:13:22 +0100

    cond

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function loadData(cardsData) {
         '  <div class="card-body">' +
         '   <h6 class="card-title productName">' + productName + '</h6>' +
         '   <h6 class="card-title title">' + title + '</h6>' +
-        '   <p class="card-title recordLabel">' + recordLabel + '<p>' +
+        '   <p class="card-title recordLabel">' + recordLabel + '</p>' +
         '   <p class="productCondition card-text">' + productCondition + '</p>' +
         ' </div>' +
         ' <div class="card-footer text-center">' +
